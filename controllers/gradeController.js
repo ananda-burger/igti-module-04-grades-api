1aeb8f15732e5d54532f42d60b9e244ccbb04f23
Ananda Burger <aburger.ananda@gmail.com>
2021-02-03 14:17:35 -0300

    Implement all API endpoints

--- controllers/gradeController.js
+++ controllers/gradeController.js
@@ -3,9 +3,20 @@ import { logger } from '../config/logger.js';
 import { Grade } from '../models/Grade.js'
 
 const create = async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: 'Dados para criacao vazio',
+    });
+  }
+
   try {
+    logger.info(`POST /grade - ${JSON.stringify(req.body)}`);
+    const gradeParams = {
+      ...req.body,
+      lastModified: new Date().toISOString()
+    }
+    const grade = await Grade.create(gradeParams)
     res.send({ message: 'Grade inserido com sucesso' });
-    logger.info(`POST /grade - ${JSON.stringify()}`);
   } catch (error) {
     res
       .status(500)
@@ -38,6 +49,8 @@ const findOne = async (req, res) => {
 
   try {
     logger.info(`GET /grade - ${id}`);
+    const grade = await Grade.findById(id)
+    res.send(grade)
   } catch (error) {
     res.status(500).send({ message: 'Erro ao buscar o Grade id: ' + id });
     logger.error(`GET /grade - ${JSON.stringify(error.message)}`);
@@ -52,9 +65,17 @@ const update = async (req, res) => {
   }
 
   const id = req.params.id;
+  const {name, subject, type, value} = req.body
 
   try {
     logger.info(`PUT /grade - ${id} - ${JSON.stringify(req.body)}`);
+    const grade = await Grade.findById(id)
+    if (name) grade.name = name
+    if (subject) grade.subject = subject
+    if (type) grade.type = type
+    if (value) grade.value = value
+    await grade.save()
+    res.send(grade)
   } catch (error) {
     res.status(500).send({ message: 'Erro ao atualizar a Grade id: ' + id });
     logger.error(`PUT /grade - ${JSON.stringify(error.message)}`);
@@ -66,6 +87,13 @@ const remove = async (req, res) => {
 
   try {
     logger.info(`DELETE /grade - ${id}`);
+    const {deletedCount} = await Grade.remove({_id: id})
+    if (deletedCount === 1) {
+      res.status(200).end()
+    } else {
+      logger.error(`DELETE /grade - Could not delete grade '${id}'`)
+      res.status(418).end() // I'm a teapot
+    }
   } catch (error) {
     res
       .status(500)
@@ -77,6 +105,8 @@ const remove = async (req, res) => {
 const removeAll = async (req, res) => {
   try {
     logger.info(`DELETE /grade`);
+    await Grade.remove()
+    res.send(200).end()
   } catch (error) {
     res.status(500).send({ message: 'Erro ao excluir todos as Grades' });
     logger.error(`DELETE /grade - ${JSON.stringify(error.message)}`);
--- models/Grade.js
+++ models/Grade.js
@@ -4,7 +4,8 @@ const Grade = mongoose.model('Grade', {
   name: {type: String, required: true},
   subject: {type: String, required: true},
   type: {type: String, required: true},
-  value: {type: String, required: true}
+  value: {type: String, required: true},
+  lastModified: {type: Date, required: true}
 })
 
 export {Grade}
